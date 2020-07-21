0e0dc258155c24a56c2f7352349220a8ec87ace5
Underglaze-Blue <lepapillonangel@gmail.com>
2020-07-21 23:11:28 +0800

    修改样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Create from "./pages/gallery/create"
 import Gallery from "./pages/gallery/gallery"
 import './App.css'
 import Poetry from "./pages/poetry/poetry";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Poetry />
-        <Create message="React"/>
-      </header>
-      <main>
-        <Gallery />
-      </main>
-    </div>
-  );
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-around;
+  width: 100vw;
+`
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <StyledHeader className="App-header">
+          <Poetry />
+          <Create message="Click Button Create Gallery"/>
+        </StyledHeader>
+        <main>
+          <Gallery />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/poetry/poetry.tsx
+++ src/pages/poetry/poetry.tsx
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
 import {fetchPoetry} from '../../api'
 import styled from "styled-components";
+import {Button} from 'antd'
 
 interface IPoetryProps {}
 
-interface IPoetryState {
+interface IPoetryCommon {
   content: string
   origin: string
   author: string
 }
 
-interface IPoetryPromise extends IPoetryState{
+interface IPoetryState extends IPoetryCommon{
+  loading: boolean
+}
+
+interface IPoetryPromise extends IPoetryCommon{
   category: string
 }
+
+const PoetryMain = styled.main`
+  flex: 0 0 30vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const StyledH3 = styled.h3`
   color: white;
@@ -28,23 +40,39 @@ class Poetry extends Component<IPoetryProps, IPoetryState> {
     this.state  = {
       content: '',
       origin: '',
-      author: ''
+      author: '',
+      loading: false
     }
   }
-  componentWillMount() {
+  _fetchPoetry = () => {
+    this.setState({
+      loading: true
+    })
     fetchPoetry().then(res => {
       const {content, origin, author} = res as IPoetryPromise
       this.setState({
         content, origin, author
       })
+    }).finally(() => {
+      this.setState({
+        loading: false
+      })
     })
+  }
+  componentWillMount() {
+    this._fetchPoetry()
   }
   render(): React.ReactElement {
     return (
-      <>
-        <StyledH3>{this.state.origin}<StyledSpan>{this.state.author && `『${this.state.author}』`}</StyledSpan></StyledH3>
-        <p>{this.state.content}</p>
-      </>
+      <PoetryMain>
+        <aside>
+          <StyledH3>{this.state.origin}<StyledSpan>{this.state.author && `『${this.state.author}』`}</StyledSpan></StyledH3>
+          <p>{this.state.content}</p>
+        </aside>
+        <Button loading={this.state.loading} onClick={this._fetchPoetry} type="primary">
+          刷新
+        </Button>
+      </PoetryMain>
     );
   }
 }
