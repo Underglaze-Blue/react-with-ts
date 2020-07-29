6acc0515d0821a9c7d0fbf3a35082119732bb5d2
Underglaze-Blue <lepapillonangel@gmail.com>
2020-07-29 17:52:12 +0800

    格式化

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,5 @@
-import {isDate, isNumber} from './typesJudge';
-import {LocalStorageType} from '../type';
+import {isDate, isNumber} from './typesJudge'
+import {LocalStorageType} from '../type'
 
 // localstorage
 /*
@@ -18,55 +18,55 @@ class LocalData implements LocalStorageType{
   constructor() {
     this.handlePeriod = (period: number | Date, now: Date) => {
       if (isDate(period) && Number(period) > Number(now)) {
-        return (Number(period)) - (Number(now));
+        return (Number(period)) - (Number(now))
       } else if (isNumber(period) && period > 0) {
-        return period as number;
+        return period as number
       } else {
-        return null;
+        return null
       }
-    };
+    }
     this.isNotExist = (data: any) => {
-      return data === null || typeof data === 'undefined';
-    };
+      return data === null || typeof data === 'undefined'
+    }
     this.isOutPeriod = (data: any) => {
-      if (Number(new Date()) - data['__writeTime__'] > data['__period__']) return true;
-      return false;
-    };
+      if (Number(new Date()) - data['__writeTime__'] > data['__period__']) return true
+      return false
+    }
   }
 
   save (name: string, data: any, period?: number | Date) {
-    const now = new Date();
+    const now = new Date()
     const saveData = {
       data,
       __writeTime__: Number(now),
       __period__: period ? this.handlePeriod(period, now) : null
-    };
-    localStorage.setItem(`${name}__with__timeliness__`, JSON.stringify(saveData));
+    }
+    localStorage.setItem(`${name}__with__timeliness__`, JSON.stringify(saveData))
   }
   get (name: string) {
-    if (!this.isNotExist(localStorage.getItem(name))) return localStorage.getItem(name);
-    const dataJSON = localStorage.getItem(`${name}__with__timeliness__`);
+    if (!this.isNotExist(localStorage.getItem(name))) return localStorage.getItem(name)
+    const dataJSON = localStorage.getItem(`${name}__with__timeliness__`)
     if(this.isNotExist(dataJSON)) {
-      return null;
+      return null
     }
-    const storageData = JSON.parse(dataJSON as string);
+    const storageData = JSON.parse(dataJSON as string)
     if(this.isNotExist(storageData['__period__'])) {
-      return storageData.data;
+      return storageData.data
     }
     if(this.isOutPeriod(storageData)) {
-      this.remove(`${name}__with__timeliness__`);
-      storageData.data = null;
+      this.remove(`${name}__with__timeliness__`)
+      storageData.data = null
     }
-    return storageData.data;
+    return storageData.data
   }
   clear () {
-    localStorage.clear();
+    localStorage.clear()
   }
   remove (name: string) {
-    localStorage.removeItem(`${name}__with__timeliness__`);
-    localStorage.removeItem(name);
+    localStorage.removeItem(`${name}__with__timeliness__`)
+    localStorage.removeItem(name)
   }
 }
 
-export default LocalData;
+export default LocalData
 
